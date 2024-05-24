86031e12a3543f9e2cabfea162d349fab7387224
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-05-24 20:30:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ for ex: 'https://www.google.com'
 
 START_BTN = InlineKeyboardMarkup(
         [[
-        InlineKeyboardButton('Source Code', url='https://github.com/samadii/WebDownloaderBot'),
+        InlineKeyboardButton('My Father', url='https://t.me/Matiz_Owner'),
         ]]
     )
 

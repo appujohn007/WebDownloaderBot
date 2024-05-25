07f11010e0bae5ef23f0387948f983e633063779
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-05-25 15:13:16 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -13,19 +13,21 @@ def save_auth_data(data):
     with open(AUTH_FILE, 'w') as file:
         json.dump(data, file, indent=4)
 
-def add_credentials(user_id, username, password):
+def add_credentials(user_id, website, username, password):
     data = load_auth_data()
-    data[str(user_id)] = {'username': username, 'password': password}
+    if str(user_id) not in data:
+        data[str(user_id)] = {}
+    data[str(user_id)][website] = {'username': username, 'password': password}
     save_auth_data(data)
 
 def get_credentials(user_id):
     data = load_auth_data()
-    return data.get(str(user_id))
+    return data.get(str(user_id), {})
 
-def remove_credentials(user_id):
+def remove_credentials(user_id, website):
     data = load_auth_data()
-    if str(user_id) in data:
-        del data[str(user_id)]
+    if str(user_id) in data and website in data[str(user_id)]:
+        del data[str(user_id)][website]
         save_auth_data(data)
         return True
     return False
